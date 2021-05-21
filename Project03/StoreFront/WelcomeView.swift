112054ea58d21747d359dce065dd645211f570c3
its.Lea <lea.wleonard@Leas-Mac.local>
2021-05-21 15:16:57 -0700

    Working UI StoreFront Section

---
+++ Project03/StoreFront/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  Project03
+//
+//  Created by Lea W. Leonard on 5/21/21.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
