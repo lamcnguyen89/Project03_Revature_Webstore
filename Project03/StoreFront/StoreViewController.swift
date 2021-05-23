d4f388d5a725bc62276cc164045a0cb94858655b
Lea.W.Leonard <lea.wleonard@revature.net>
2021-05-23 15:37:29 -0700

    Working on Storefront UI & Functions

---
+++ Project03/StoreFront/StoreViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StoreViewController.swift
+//  Project03
+//
+//  Created by Lea W. Leonard on 5/22/21.
+//
+
+import UIKit
+
+class StoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Project03/ViewController.swift
+++ Project03/ViewController.swift
@@ -12,8 +12,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       
     }
-
-
+    
+/*
+    @IBAction func getStore(_ sender: Any) {
+        
+        let initialView = UIStoryboard(name: "StoreBoard", bundle: nil)
+        
+        // create connection to the view controller you want to show from
+        // initial StoryBoard.View
+        let StoreView = initialView.instantiateViewController(withIdentifier: "vwStore") as! StoreViewController
+        
+        self.present(StoreView, animated: true, completion: nil)
+    }
+ */
+    
 }
 
