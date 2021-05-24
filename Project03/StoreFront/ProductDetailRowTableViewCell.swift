436a956f0f1c070a9779cafa727d78da00b67cb1
Lea.W.Leonard <lea.wleonard@revature.net>
2021-05-24 14:43:27 -0700

    add product detail view

--- Project03/StoreFront/ProductDetailRowTableViewCell.swift
+++ Project03/StoreFront/ProductDetailRowTableViewCell.swift
@@ -19,6 +19,7 @@ class ProductDetailRowTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
