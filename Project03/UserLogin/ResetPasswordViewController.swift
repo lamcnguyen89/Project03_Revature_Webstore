406cb6210d6909bbdf1a620b4cf18262022f20a2
Lam Nguyen <lamcnguyen89@gmail.com>
2021-05-27 03:14:06 -0400

    Added some of the functionality to the login view and added some outlets to the reset view

--- Project03/UserLogin/ResetPasswordViewController.swift
+++ Project03/UserLogin/ResetPasswordViewController.swift
@@ -8,22 +8,25 @@
 import UIKit
 
 class ResetPasswordViewController: UIViewController {
-
+    
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var newPassword: UITextField!
+    @IBOutlet weak var confirmPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func back(_ sender: Any) {
     }
-    */
+    
+    @IBAction func submit(_ sender: Any) {
+    }
+    
 
 }
