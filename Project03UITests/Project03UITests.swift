725f1d8fcee3f676ddc74097ad8dd3e687718c5c
Lam Nguyen <lamcnguyen89@gmail.com>
2021-05-27 00:17:23 -0400

    Added a UITest to just check general functionality

--- Project03UITests/Project03UITests.swift
+++ Project03UITests/Project03UITests.swift
@@ -8,35 +8,36 @@
 import XCTest
 
 class Project03UITests: XCTestCase {
+    var app: XCUIApplication!
 
+    // Setup code for the test(s)
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        try super.setUpWithError()
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
+        app = XCUIApplication()
+        app.launch()
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    // Test General UI Functionality
+    func testShoppingCartButton() {
+        
+        app/*@START_MENU_TOKEN@*/.staticTexts["    Menu"]/*[[".buttons[\"    Menu\"].staticTexts[\"    Menu\"]",".staticTexts[\"    Menu\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let scrollViewsQuery = app.scrollViews
+        scrollViewsQuery.otherElements.tables.staticTexts["First Product"].swipeDown()
+        scrollViewsQuery.otherElements.staticTexts["[Category]"].tap()
+        app/*@START_MENU_TOKEN@*/.scrollViews.containing(.other, identifier:"Vertical scroll bar, 1 page").element/*[[".scrollViews.containing(.other, identifier:\"Horizontal scroll bar, 1 page\").element",".scrollViews.containing(.other, identifier:\"Vertical scroll bar, 1 page\").element"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        scrollViewsQuery.children(matching: .other).element(boundBy: 0).children(matching: .other).element.swipeUp()
+        app/*@START_MENU_TOKEN@*/.staticTexts["   Cart"]/*[[".buttons[\"   Cart\"].staticTexts[\"   Cart\"]",".staticTexts[\"   Cart\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.tables.children(matching: .cell).matching(identifier: "test, 2").element(boundBy: 1).otherElements.containing(.image, identifier:"test").element.swipeUp()
+        app.buttons["    Menu"].tap()
+        
     }
+    
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+
 }
