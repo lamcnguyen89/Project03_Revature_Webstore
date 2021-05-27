905f5071d3b503d2f397bbee8b8e12c11156b9d0
Lam Nguyen <lamcnguyen89@gmail.com>
2021-05-27 13:26:32 -0400

    Got the Reset Password View, Login View, and Create User View to function without bugs

--- Project03/UserLogin/SignUpViewController.swift
+++ Project03/UserLogin/SignUpViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var confirmPassword: UITextField!
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +25,59 @@ class SignUpViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
+    @IBAction func submit(_ sender: Any) {
+        
+        
+        if email.text != "" &&  username.text != "" && password.text != "" && confirmPassword.text != "" && firstName.text != "" && lastName.text != ""
+            {
+            
+            if password.text == confirmPassword.text {
+                
+                let dic = ["email" : email.text , "pass" : password.text , "fname" : firstName.text, "lname" : lastName.text , "name" : username.text  ]
+                
+                DataDelegate.inst.createUser(dic as! [String:String])
+                
+                let sb : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                
+                // This instantiates or shows the storyboard object/view controller that you want to show once you log in.
+                let log = sb.instantiateViewController(withIdentifier: "Login") as! LoginViewController
+                
+                // Function which belongs to UIviewController. If this isn't called, the next screen will not show up.
+                // This shows the screen once you log on.
+                self.present(log, animated: true, completion: nil)
+                
+                email.text = ""
+                username.text = ""
+                firstName.text = ""
+                lastName.text = ""
+                password.text = ""
+                confirmPassword.text = ""
+                outputLabel.text = ""
+                
+            } else {
+                print("Password fields do not match")
+                outputLabel.text = "Password fields do not match"
+                password.text = ""
+                confirmPassword.text = ""
+                
+            }
+    
+        } else {
+            print("Fill out all textfields")
+            outputLabel.text = "Fill out all textfields"
+        }
+        
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
+    
+    @IBAction func back(_ sender: Any) {
+        let sb : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let wel = sb.instantiateViewController(withIdentifier: "Login") as! LoginViewController
+        self.present(wel, animated: true, completion: nil)
+        
+    }
+    
+
 
 }
