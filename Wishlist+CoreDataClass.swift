fd84789b1ed7a94a713bcf28e6fc8c930e832740
Kirtland Ross <kirtlandross@gmail.com>
2021-05-28 17:14:25 -0400

    Added wishlist entity

---
+++ Wishlist+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Wishlist+CoreDataClass.swift
+//  Project03
+//
+//  Created by Kirtland Ross on 5/28/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Wishlist)
+public class Wishlist: NSManagedObject {
+
+}
