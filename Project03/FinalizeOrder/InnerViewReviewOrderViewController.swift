33f51c8d7271f87d521fe17cb7c770a74c583ffe
Lam Nguyen <lamcnguyen89@gmail.com>
2021-05-31 11:58:07 -0400

    Added functionality for calculating total price into the View portion of the code. All we need to do is to hook it up to CoreData

--- Project03/FinalizeOrder/InnerViewReviewOrderViewController.swift
+++ Project03/FinalizeOrder/InnerViewReviewOrderViewController.swift
@@ -9,21 +9,58 @@ import UIKit
 
 class InnerViewReviewOrderViewController: UIViewController {
 
+    @IBOutlet weak var calculatedTotal: UILabel!
+    var totalPrice: Double = 0
+    var taxes: Double = 0
+    var shipping: Double = 20
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        calculateOrder()
+    }
+    
+    @IBAction func submitOrder(_ sender: Any) {
+        
+        // Add a marker to the database showing that an order has been submitted, what was placed in the order and so on and so on.
+        
+        // Redirect to Confirmation page
+        let sb : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let wel = sb.instantiateViewController(withIdentifier: "Confirmation") as! OrderConfirmationViewController
+        self.present(wel, animated: true, completion: nil)
+    }
+    
+    // Function to calculate the order total
+    func calculateOrder() {
+        
+        // Call the object that shows the user's orders. Right now I'm just using a placeholder object to write and test this loop.
+        let orderData = [
+            0 : ["price" : 25.25, "quantity" : 2],
+            1 : ["price" : 100.36, "quantity" : 5],
+            2 : ["price" : 300.44, "quantity" : 13],
+            3 : ["price" : 200.2345, "quantity" : 8]
+        ]
+        
+        var perItemPriceArray = [Double]()
+        
+        var i:Int = 0
+        
+        while i < orderData.count {
+            
+            let itemSubtotal = orderData[i]!["price"]! * orderData[i]!["quantity"]!
+            perItemPriceArray.append(itemSubtotal)
+            i += 1
+            
+        }
+        
+        totalPrice = perItemPriceArray.reduce(0, +) + taxes + shipping
+        let priceString : String = String(format: "%.2f", totalPrice)
+        calculatedTotal.text = "$\(priceString)"
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
