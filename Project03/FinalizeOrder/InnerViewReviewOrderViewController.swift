945bff7949d226d7fb2722c3fb8967f7cfde1947
Lam Nguyen <lamcnguyen89@gmail.com>
2021-06-01 17:15:04 -0400

    Uploading broken test for Kirkland to Troubleshoot

--- Project03/FinalizeOrder/InnerViewReviewOrderViewController.swift
+++ Project03/FinalizeOrder/InnerViewReviewOrderViewController.swift
@@ -13,18 +13,23 @@ class InnerViewReviewOrderViewController: UIViewController {
     @IBOutlet weak var taxesLabel: UILabel!
     @IBOutlet weak var shippingLabel: UILabel!
     @IBOutlet weak var calculatedTotal: UILabel!
-    var totalPrice: Double = 0
-    var taxes: Double = 0
-    var shipping: Double = 20
+
     
+    let orderData = [
+        0 : ["price" : 25.25, "quantity" : 2],
+        1 : ["price" : 100.36, "quantity" : 5],
+        2 : ["price" : 300.44, "quantity" : 13],
+        3 : ["price" : 200.2345, "quantity" : 8]
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        taxesLabel.text = "Taxes: $\(String(format: "%.2f", taxes))"
-        shippingLabel.text = "Flate Rate Shipping: $\(String(format: "%.2f", shipping))"
-        calculateOrder()
+        
+        let total = calculateOrder(orderData)
+        let priceString : String = String(format: "%.2f", total)
+        calculatedTotal.text = priceString
     }
     
     @IBAction func submitOrder(_ sender: Any) {
@@ -38,15 +43,13 @@ class InnerViewReviewOrderViewController: UIViewController {
     }
     
     // Function to calculate the order total
-    func calculateOrder() {
+    func calculateOrder(_ orders: [Int : [String : Double]])-> Double  {
+        
+        var totalPrice: Double = 0
+        let taxes: Double = 0
+        let shipping: Double = 20
         
         // Call the object that shows the user's orders. Right now I'm just using a placeholder object to write and test this loop.
-        let orderData = [
-            0 : ["price" : 25.25, "quantity" : 2],
-            1 : ["price" : 100.36, "quantity" : 5],
-            2 : ["price" : 300.44, "quantity" : 13],
-            3 : ["price" : 200.2345, "quantity" : 8]
-        ]
         
         var perItemPriceArray = [Double]()
         
@@ -61,8 +64,11 @@ class InnerViewReviewOrderViewController: UIViewController {
         }
         
         totalPrice = perItemPriceArray.reduce(0, +) + taxes + shipping
-        let priceString : String = String(format: "%.2f", totalPrice)
-        calculatedTotal.text = "$\(priceString)"
+        
+        taxesLabel.text = "Taxes: $\(String(format: "%.2f", taxes))"
+        shippingLabel.text = "Flate Rate Shipping: $\(String(format: "%.2f", shipping))"
+        
+        return totalPrice
         
     }
     
