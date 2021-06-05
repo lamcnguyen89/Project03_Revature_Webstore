6ac49b01033726f14b6d5dbf28d2213072225400
Lam Nguyen <lamcnguyen89@gmail.com>
2021-06-05 19:39:04 -0400

    Fixed formatting error when pressing a button in swift and not having the view change.

--- Project03/SwiftUI/ShoppingCartScrollView.swift
+++ Project03/SwiftUI/ShoppingCartScrollView.swift
@@ -7,41 +7,51 @@
 
 import SwiftUI
 import CoreData
+import UIKit
 
 struct ShoppingCartScrollView: View {
     @State private var showProduct = false
+    @State var goToCheckout = false
+    var shoppingCartItems = [
+        0 : ["name" : "Nvidia 3060Ti Eagle", "price" : "600" , "image" : "3060TiEagle"],
+        1 : ["name" : "Nvidia 3070 Founders" , "price" : "800" , "image" : "3070Founders"],
+        2 : ["name" : "Nvidia 3080FTW", "price" : "1200" , "image" : "3080FTW"],
+        3 : ["name" : "Nvidia 6700XT" , "price" : "700" , "image" : "6700XT"]
+    
+    ]
     
     var body: some View {
         Form{
-            ForEach(0..<10){ i in
+            ForEach(0..<shoppingCartItems.count){ i in
                 Button(action: {showProduct=true}) {
                     HStack{
-                        Image("test")
+                        Image(shoppingCartItems[i]!["image"]!)
                             .resizable()
-                            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        Text("Test: " + String(i))
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        Spacer()
+                        
+                        VStack(alignment: .leading){
+                            Text("Item: \(shoppingCartItems[i]!["name"]!)")
+                            Spacer()
+                            Text("Price: \(shoppingCartItems[i]!["price"]!)")
+                        }.padding()
                     }
                 }
             }
-            Button(action: {checkout()}) {
+            Button(action: {goToCheckout=true}) {
                 HStack{
                     Spacer()
                     Text("Checkout").frame( height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                     Spacer()
                 }
-            }
+            }.buttonStyle(BorderlessButtonStyle())
+            .sheet(isPresented: $goToCheckout, content: {
+                ViewControllerAsUIView(storyboard: "Main", VC: "Checkout", prodData: nil)
+            })
         }
-        .sheet(isPresented: $showProduct, content: {
-            ViewControllerAsUIView(storyboard: "Main", VC: "ProductDetail", prodData: nil)
-        })
-    }
-    func checkout(){
-        
+
     }
 }
 
-//struct ShoppingCartScrollView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ShoppingCartScrollView()
-//    }
-//}
+
