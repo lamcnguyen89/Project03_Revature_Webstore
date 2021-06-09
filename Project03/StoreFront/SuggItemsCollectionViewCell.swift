ab6eb3b4d12cbeac1665fa609d7490d4c46fc840
Lea.W.Leonard <lea.wleonard@revature.net>
2021-06-09 07:30:12 -0400

    Suggested items Scroll ; just needs button interaction

---
+++ Project03/StoreFront/SuggItemsCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SuggItemsCollectionViewCell.swift
+//  Project03
+//
+//  Created by Lea W. Leonard on 6/9/21.
+//
+
+import UIKit
+
+class SuggItemsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgProduct: UIImageView!    
+    @IBOutlet weak var lblDesc: UILabel!    
+    @IBOutlet weak var btnGetProduct: UIButton!
+}
